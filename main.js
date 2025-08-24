6f417edd77a4479fef9219e622b1ede285d9f685
Natolium <ent.ko@outlook.jp>
2025-08-24 18:10:29 +0900

    Update main.js

--- main.js
+++ main.js
@@ -54,10 +54,11 @@ function safeMessage (msg, text, type = 'send', silent = false, delay = 0) { // 
 }
 
 client.on(Events.MessageCreate, msg => {
+  console.log(msg);
   if (msg.author.bot) { return; }
 
   if (msg.mentions.users.has(client.user.id)) {
-    safeMessage(msg, ':sob:', true, TYPING_DELAY);
+    safeMessage(msg, ':sob:', 'send', true, TYPING_DELAY);
   }
 
   if (msg.content === 'ping') {
