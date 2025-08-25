808f080d542fdf91ba9519a0818b69a13a1ea147
Natolium <ent.ko@outlook.jp>
2025-08-25 16:44:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ module.exports = function(client) {
     client.on(Events.MessageCreate, (msg) => {
       if (msg.author.bot) return;
 
-      if (msg.mentions.users.has(client.user.id)) {
+      if (msg.mentions.users.has(client.user)) {
         SafeMessage(msg, ':sob:', 'send', true, TYPING_DELAY);
       }
 
