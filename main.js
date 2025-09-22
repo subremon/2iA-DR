d29cbd5329c5e299699bc79028da4cc9ef82d3f6
Natolium <ent.ko@outlook.jp>
2025-09-22 18:41:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ module.exports = function(client) {
     client.on(Events.MessageCreate, (msg) => {
       if (msg.author.bot) return;
 
-      if (/(-?\d+)?([+\-*\/]\d+)*([+\-*\/]?(?<![a-zA-Z])\d*[R]\d+)+(?:([+\-*\/]\d+)|([+\-*\/](?<![a-zA-Z])\d*[R]\d+))*/i.test(msg.content)) {
+      if (/(?<![A-Za-z0-9])(?:-?\d+)?(?:[+\-*/]\d+)*(?:[+\-*/]?\d*R\d+)+(?:[+\-*/]\d+|[+\-*/]\d*R\d+)*(?![A-Za-z0-9])/i.test(msg.content)) {
         SafeMessage(client, msg, BasicDice(msg.content)[0]);
       }
 
